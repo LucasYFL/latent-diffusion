f1fbb0f18e4208c7223e313d2f4c1fa7d35d368e
LucasYFL <90341110+LucasYFL@users.noreply.github.com>
2023-10-19 13:55:10 -0400

    save frequently

--- main.py
+++ main.py
@@ -576,7 +576,7 @@ if __name__ == "__main__":
                 "filename": "{epoch:06}-{step:09}",
                 "verbose": True,
                 "save_last": True,
-                'every_n_train_steps': 5000,
+                'every_n_train_steps': 500,
             }
         }
         if hasattr(model, "monitor"):
@@ -645,7 +645,7 @@ if __name__ == "__main__":
                          "filename": "{epoch:06}-{step:09}",
                          "verbose": True,
                          'save_top_k': 3,
-                         'every_n_train_steps': 5000,
+                         'every_n_train_steps': 500,
                          'save_weights_only': True
                      }
                      }

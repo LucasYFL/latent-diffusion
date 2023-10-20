88bda197c53cbb26a3b9eb3faecb5970ad43162c
LucasYFL <lucas_lu0323@hotmail.com>
2023-10-20 02:31:24 -0400

    minor change

--- main.py
+++ main.py
@@ -363,8 +363,8 @@ class ImageLogger(Callback):
             self.log_local(pl_module.logger.save_dir, split, images,
                            pl_module.global_step, pl_module.current_epoch, batch_idx)
 
-            logger_log_images = self.logger_log_images.get(logger, lambda *args, **kwargs: None)
-            logger_log_images(pl_module, images, pl_module.global_step, split)
+            # logger_log_images = self.logger_log_images.get(logger, lambda *args, **kwargs: None)
+            # logger_log_images(pl_module, images, pl_module.global_step, split)
 
             if is_train:
                 pl_module.train()
@@ -396,9 +396,9 @@ class CUDACallback(Callback):
     # see https://github.com/SeanNaren/minGPT/blob/master/mingpt/callback.py
     def on_train_epoch_start(self, trainer, pl_module):
         # Reset the memory use counter
-        # torch.cuda.reset_peak_memory_stats(trainer.root_gpu)
-        # torch.cuda.synchronize(trainer.root_gpu)
-        # self.start_time = time.time()
+        torch.cuda.reset_peak_memory_stats(trainer.root_gpu)
+        torch.cuda.synchronize(trainer.root_gpu)
+        self.start_time = time.time()
         torch.cuda.empty_cache()
         gc.collect()
 
@@ -406,17 +406,17 @@ class CUDACallback(Callback):
         torch.cuda.synchronize(trainer.root_gpu)
         torch.cuda.empty_cache()
         gc.collect()
-        # max_memory = torch.cuda.max_memory_allocated(trainer.root_gpu) / 2 ** 20
-        # epoch_time = time.time() - self.start_time
+        max_memory = torch.cuda.max_memory_allocated(trainer.root_gpu) / 2 ** 20
+        epoch_time = time.time() - self.start_time
 
-        # try:
-        #     max_memory = trainer.training_type_plugin.reduce(max_memory)
-        #     epoch_time = trainer.training_type_plugin.reduce(epoch_time)
+        try:
+            max_memory = trainer.training_type_plugin.reduce(max_memory)
+            epoch_time = trainer.training_type_plugin.reduce(epoch_time)
 
-        #     rank_zero_info(f"Average Epoch time: {epoch_time:.2f} seconds")
-        #     rank_zero_info(f"Average Peak memory {max_memory:.2f}MiB")
-        # except AttributeError:
-        #     pass
+            rank_zero_info(f"Average Epoch time: {epoch_time:.2f} seconds")
+            rank_zero_info(f"Average Peak memory {max_memory:.2f}MiB")
+        except AttributeError:
+            pass
         
 
 
@@ -578,16 +578,16 @@ if __name__ == "__main__":
             "target": "pytorch_lightning.callbacks.ModelCheckpoint",
             "params": {
                 "dirpath": ckptdir,
-                "filename": "{epoch:06}-{step:09}",
+                "filename": "{epoch:06}",
                 "verbose": True,
                 "save_last": True,
-                'every_n_epochs':10
+                'every_n_epochs':5
             }
         }
-        # if hasattr(model, "monitor"):
-        #     print(f"Monitoring {model.monitor} as checkpoint metric.")
-        #     default_modelckpt_cfg["params"]["monitor"] = model.monitor
-        #     default_modelckpt_cfg["params"]["save_top_k"] = 3
+        if hasattr(model, "monitor"):
+            print(f"Monitoring {model.monitor} as checkpoint metric.")
+            default_modelckpt_cfg["params"]["monitor"] = model.monitor
+            default_modelckpt_cfg["params"]["save_top_k"] = 3
 
         if "modelcheckpoint" in lightning_config:
             modelckpt_cfg = lightning_config.modelcheckpoint
